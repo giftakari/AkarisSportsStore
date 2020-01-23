7710f68577d254766f568526e2e9215807178a19
Godsgift ThankGod Akari <giftakari@yahoo.com>
2020-01-23 12:09:36 +1100

    Added Model to the project

---
+++ src/app/model/product.repository.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { Product } from "./product.model";
+import { StaticDataSource } from "./static.datasource";
+
+@Injectable()
+export class ProductRepository {
+  private products: Product[] = [];
+  private categories: string[] = [];
+
+  constructor(private dataSource: StaticDataSource) {
+    dataSource.getProducts().subscribe(data => {
+      this.products = data;
+      this.categories = data
+        .map(product => product.category)
+        .filter((cat, index, array) => array.indexOf(cat) == index)
+        .sort();
+    });
+  }
+  getProducts(category: string = null): Product[] {
+    return this.products.filter(
+      product => category == null || category == product.category
+    );
+  }
+}
