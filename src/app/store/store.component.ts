f60085910273a3a9c448caa4cfaaa6d5dc212a18
Godsgift ThankGod Akari <giftakari@yahoo.com>
2020-01-23 19:47:06 +1100

    Cart functionality

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -8,13 +8,51 @@ import { ProductRepository } from "../model/product.repository";
   styleUrls: ["store.component.css"]
 })
 export class StoreComponent {
+  public selectedCategory = null;
+  public productsPerpage = 4;
+  public selectedPage = 1;
+
   constructor(private repository: ProductRepository) {}
 
   get products(): Product[] {
-    return this.repository.getProducts();
+    let pageIndex = (this.selectedPage - 1) * this.productsPerpage;
+    return this.repository
+      .getProducts(this.selectedCategory)
+      .slice(pageIndex, pageIndex + this.productsPerpage);
   }
 
   get categories() {
     return this.repository.getCatogories();
+  }
+
+  changeCategory(newCategory?: string) {
+    this.selectedCategory = newCategory;
+  }
+
+  changePage(newPage: number) {
+    this.selectedPage = newPage;
+  }
+
+  changePageSize(newSize: number) {
+    this.productsPerpage = Number(newSize);
+    this.changePage(1);
+  }
+
+  /*  get pageNumber(): number[] {
+    return Array(
+      Math.ceil(
+        this.repository.getProducts(this.selectedCategory).length /
+          this.productsPerpage
+      )
+    )
+      .fill(0)
+      .map((number, index) => index + 1);
+  } */
+
+  get PageCount(): number {
+    return Math.ceil(
+      this.repository.getProducts(this.selectedCategory).length /
+        this.productsPerpage
+    );
   }
 }
